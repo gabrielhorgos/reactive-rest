74c97df9d3d96f6e3af23a87fc13922ef885dab1
Gabriel Horgos <horgos.gabriel@gmail.com>
2018-08-22 11:52:42 +0300

    Add swagger core

--- src/main/java/sample/reactive/application/ApplicationConfig.java
+++ src/main/java/sample/reactive/application/ApplicationConfig.java
@@ -1,11 +1,26 @@
 package sample.reactive.application;
 
 
+import io.swagger.v3.jaxrs2.integration.resources.AcceptHeaderOpenApiResource;
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import sample.reactive.rest.business.boundary.AsyncApiResource;
 
 import javax.ws.rs.core.Application;
 import java.util.Set;
 
+@OpenAPIDefinition(info =
+@Info(
+        title = "Reactive Rest sample application",
+        version = "0.0",
+        description = "Reactive-REST",
+        license = @License(name = "Reactive-REST 1.0", url = "http://foo.bar"),
+        contact = @Contact(url = "http://gigantic-server.com", name = "Fred", email = "Fred@gigagantic-server.com")
+)
+)
 @javax.ws.rs.ApplicationPath("/rest")
 public class ApplicationConfig extends Application {
 
@@ -22,6 +37,10 @@ public class ApplicationConfig extends Application {
 
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(AsyncApiResource.class);
+
+        //register JAX-RS openapi resources
+        resources.add(OpenApiResource.class);
+        resources.add(AcceptHeaderOpenApiResource.class);
     }
 
 }
---
+++ src/main/java/sample/reactive/rest/business/boundary/RegistrationReponse.java
@@ -0,0 +1,12 @@
+package sample.reactive.rest.business.boundary;
+
+public class RegistrationReponse {
+
+    public String key;
+    public String value;
+
+    public RegistrationReponse(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+}
